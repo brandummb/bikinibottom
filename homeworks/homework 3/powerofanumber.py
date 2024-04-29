f7e16b0e7505dad3d12da04c4d4f879945b5ddcd
brandummb <157636153+brandummb@users.noreply.github.com>
2024-04-28 19:14:12 -0700

    Delete homeworks directory

--- homeworks/homework 3/powerofanumber.py
+++
@@ -1,9 +0,0 @@
-def powerofanumber():
-    x = int(input("base:" ))
-    y = int(input("power:" ))
-    z = x
-    for n in range(1,y):
-        x *= z
-    print(x)
-
-powerofanumber()
--- homeworks/practice.py
+++
@@ -1,5 +0,0 @@
-number = 13
-import math
-log = math.log(13, 10)
-print(log)
-
